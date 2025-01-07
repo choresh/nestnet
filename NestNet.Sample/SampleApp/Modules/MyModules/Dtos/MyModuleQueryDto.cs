b984b9c216385434eae9720512bd48dd8e0fa51c
Raanan Choresh <raanan.choresh@global-e.com>
2025-01-07 11:33:47 +0200

    Update MyModuleQueryDto.cs

--- NestNet.Sample/SampleApp/Modules/MyModules/Dtos/MyModuleQueryDto.cs
+++ NestNet.Sample/SampleApp/Modules/MyModules/Dtos/MyModuleQueryDto.cs
@@ -7,7 +7,7 @@ namespace SampleApp.Modules.MyModules.Dtos
     /// </summary>
     public class MyModuleQueryDto
     {
-		// public Int32? MyModuleId { get; set; }
+		public Int32? MyModuleId { get; set; }
 		public String? Name { get; set; }
 		public Int32? Age { get; set; }
 		public String? Email { get; set; }
