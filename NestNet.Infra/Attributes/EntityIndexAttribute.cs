3ae046ca3d59b9e663516e2103b71b181087ae92
Raanan Choresh <raanan.choresh@global-e.com>
2025-01-22 22:53:21 +0200

    Create EntityIndexAttribute.cs

---
+++ NestNet.Infra/Attributes/EntityIndexAttribute.cs
@@ -0,0 +1,21 @@
+﻿#pragma warning disable IDE0290 // Use primary constructor
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NestNet.Infra.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class EntityIndexAttribute : Attribute
+    {
+        private readonly IndexAttribute _indexAttribute;
+
+        public EntityIndexAttribute(string propertyName, bool isUnique)
+        {
+            _indexAttribute = new IndexAttribute(propertyName);
+            _indexAttribute.IsUnique = isUnique;
+        }
+    }
+}
+
+#pragma warning restore IDE0290 // Use primary constructor
+
