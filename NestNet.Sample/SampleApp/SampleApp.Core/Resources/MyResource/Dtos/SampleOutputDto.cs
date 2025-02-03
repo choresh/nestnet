3a6f42af25efa4548a1f65873a58f5448f3eaec1
Raanan Choresh <raanan.choresh@global-e.com>
2025-02-03 15:45:34 +0200

    Fix tests.

---
+++ NestNet.Sample/SampleApp/SampleApp.Core/Resources/MyResource/Dtos/SampleOutputDto.cs
@@ -0,0 +1,10 @@
+namespace SampleApp.Core.Resources.MyResource.Dtos
+{
+    /// <summary>
+    /// This is a sample DTO class.
+    /// </summary>
+    public class SampleOutputDto
+    {
+        // Add here your required properties.
+    }
+}
